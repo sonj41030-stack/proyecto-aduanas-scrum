7f119c0860feb1a50e8a34ceea54527f20903018
Edixon Cabriless <sonj41030@gmail.com>
2026-06-05 15:50:31 -0400

    feat: api-gateway implementado con proxy manual

---
+++ api-gateway/api-wateway/src/main/java/com/duanas/apiwateway/ApiWatewayApplication.java
@@ -0,0 +1,12 @@
+package com.duanas.apiwateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiWatewayApplication.class, args);
+    }
+}
---
+++ api-gateway/api-wateway/src/test/java/com/duanas/apiwateway/ApiWatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duanas.apiwateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
